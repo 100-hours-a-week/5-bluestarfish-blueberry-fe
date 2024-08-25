612a66016d594a1a1c5e899a16dcb763301b65f9
김성현 <kimseonghyun@gimseonghyeon-ui-MacBookPro-2.local>
2024-08-25 19:28:36 +0900

    fix: mock data

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,16 @@
 import Header from "../components/common/Header";
 import MainPageContainer from "../components/Container/MainPageContainer";
 import Footer from "../components/common/Footer";
+import { useSetUserInfo } from "../utils/auth";
+import { useEffect } from "react";
 
 type MainPageProps = {};
 
 const MainPage: React.FC<MainPageProps> = () => {
+  const { setUserInfo } = useSetUserInfo();
+  useEffect(()=> {
+    setUserInfo();
+  },[])
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
--- src/pages/StudyRoomsListPage.tsx
+++ src/pages/StudyRoomsListPage.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/common/Header';
 import StudyRoomsListContainer from '../components/Container/StudyRoomsListContainer';
 import Footer from '../components/common/Footer';
+import { useSetUserInfo } from "../utils/auth";
 
 const StudyRoomsListPage: React.FC = () => {
+  const { setUserInfo } = useSetUserInfo();
+  useEffect(() => {
+    setUserInfo();
+  }, []);
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
