5d0f5c0c975f92273b020fb72c1e9be205995607
김성현 <kimseonghyun@gimseonghyeon-ui-MacBookPro-2.local>
2024-08-27 16:49:43 +0900

    fix: npm run build

--- src/components/rooms/SmallUserDisplay.tsx
+++ src/components/rooms/SmallUserDisplay.tsx
@@ -13,12 +13,7 @@ interface SmallUserDisplayProps {
 
 const SmallUserDisplay: React.FC<SmallUserDisplayProps> = ({ userStatus }) => {
   return (
-    <div
-      className="w-[400px] h-[230px] bg-white bg-cover rounded-[20px] shadow-lg"
-      style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/assets/images/user-display-default.png})`,
-      }}
-    >
+    <div className="w-[400px] h-[230px] bg-white bg-cover rounded-[20px] shadow-lg">
       <div className="top-0 left-0 w-full h-full flex items-end justify-between p-3">
         <p className="text-[16px]">{userStatus.nickname}</p>
         <button>
