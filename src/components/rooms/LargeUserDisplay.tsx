696fbfbb4e3359d2c128e70247cd453296266cb2
김성현 <kimseonghyun@gimseonghyeon-ui-MacBookPro-2.local>
2024-08-29 16:34:46 +0900

    temp

--- src/components/rooms/LargeUserDisplay.tsx
+++ src/components/rooms/LargeUserDisplay.tsx
@@ -1,3 +1,6 @@
+import { useDeviceStore } from "../../store/store";
+import { useRef, useEffect } from "react";
+
 type LargeUserDisplayProps = {
   backgroundImage: string;
 };
@@ -5,14 +8,55 @@ type LargeUserDisplayProps = {
 const LargeUserDisplay: React.FC<LargeUserDisplayProps> = ({
   backgroundImage,
 }) => {
+  const { camEnabled, speakerEnabled } = useDeviceStore();
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    if (camEnabled) {
+      // 카메라가 켜졌을 때 로컬 비디오 스트림을 가져와서 비디오 요소에 설정
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          if (videoRef.current) {
+            videoRef.current.srcObject = stream;
+            videoRef.current.play();
+          }
+        })
+        .catch((error) => {
+          console.error("Error accessing camera: ", error);
+        });
+      return () => {
+        if (videoRef.current && videoRef.current.srcObject) {
+          (videoRef.current.srcObject as MediaStream)
+            .getTracks()
+            .forEach((track) => track.stop());
+          videoRef.current.srcObject = null;
+        }
+      };
+    } else {
+      // 카메라가 꺼졌을 때 비디오 스트림을 제거
+      if (videoRef.current && videoRef.current.srcObject) {
+        (videoRef.current.srcObject as MediaStream)
+          .getTracks()
+          .forEach((track) => track.stop());
+        videoRef.current.srcObject = null;
+      }
+    }
+  }, [camEnabled]);
+
   return (
     <div
       className="w-[800px] h-[450px] bg-cover rounded-[20px] shadow-lg"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
-      <div className="top-0 left-0 w-full h-full flex items-end justify-start">
-        <p className="m-5 ml-8 text-[16px]">닉네임</p>
-      </div>
+      {camEnabled && (
+        <video
+          className="w-full h-full object-cover rounded-[20px] transform scale-x-[-1]"
+          ref={videoRef}
+          autoPlay
+          muted
+        />
+      )}
     </div>
   );
 };
