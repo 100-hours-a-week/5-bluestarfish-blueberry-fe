24e79d5f7cc8e94f4e37c5cc4459f574b9408097
yeji0214 <1225yj50@naver.com>
2024-09-08 00:36:58 +0900

    ✨ Complete MyInfoPage Layout & Validation

--- src/components/Container/MyInfoContainer.tsx
+++ src/components/Container/MyInfoContainer.tsx
@@ -1,6 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ChangeEvent } from "react";
 import TabBar from "../posts/TabBar";
 import axiosInstance from "../../utils/axiosInstance";
+import ToastNotification from "../common/ToastNotification";
+import ProfileSection from "../MyInfo/ProfileSection";
+import NicknameSection from "../MyInfo/NicknameSection";
+import PasswordSection from "../MyInfo/PasswordSection";
+import { validateProfileImage, validateNickname, validateUserPassword, validatePasswordMatch } from "../../utils/validation";
 
 const tabData = [
     { name: '나의 정보', icon: `${process.env.PUBLIC_URL}/assets/images/info-icon.png` },
@@ -21,6 +26,26 @@ function getErrorMessage(error: unknown): string {
 const MyInfoContainer: React.FC = () => {
     const [activeTab, setActiveTab] = useState<number>(0);
     const [currentUser, setCurrentUser] = useState<any | null>(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [nickname, setNickname] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
+    const [profileImagePreview, setProfileImagePreview] = useState<string | null>(null); // 프로필 이미지 미리보기 상태
+    const [nicknameHelperText, setNicknameHelperText] = useState<string>("");
+    const [nicknameHelperTextColor, setNicknameHelperTextColor] = useState<string>("text-red-500");
+    const [showNicknameSuccessToast, setShowNicknameSuccessToast] = useState(false);
+    const [showNicknameErrorToast, setShowNicknameErrorToast] = useState(false);
+    const [profileImageError, setProfileImageError] = useState("");
+    const [nicknameError, setNicknameError] = useState("* 닉네임 중복 검사를 진행해주세요.");
+    const [passwordError, setPasswordError] = useState("* 헬퍼텍스트");
+    const [confirmPasswordError, setConfirmPasswordError] = useState("* 헬퍼텍스트");
+    const [isValidNickname, setIsValidNickname] = useState(false);
+    const [isValidPassword, setIsValidPassword] = useState(false);
+    const [isPasswordMatch, setIsPasswordMatch] = useState(false);
+    const [isValidProfileImage, setIsValidProfileImage] = useState(true);
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const defaultProfileImage = `${process.env.PUBLIC_URL}/assets/images/gunssakdo.png`; // 기본 프로필 이미지 URL
 
     useEffect(() => {
         const fetchCurrentUser = async () => {
@@ -29,7 +54,8 @@ const MyInfoContainer: React.FC = () => {
                     `${process.env.REACT_APP_API_URL}/api/v1/users/whoami`
                 );
                 setCurrentUser(response.data.data);
-                console.log(response.data.data);
+                setNickname(response.data.data.nickname);
+                setProfileImagePreview(response.data.data.profileImage); // 초기 프로필 이미지 설정
             } catch (error: unknown) {
                 console.error("사용자 정보를 가져오는 데 실패했습니다:", getErrorMessage(error));
             }
@@ -37,6 +63,136 @@ const MyInfoContainer: React.FC = () => {
 
         fetchCurrentUser();
     }, []);
+
+    useEffect(() => {
+        const isFormValidNow = isValidNickname && isValidPassword && isPasswordMatch && isValidProfileImage;
+        console.log(isValidProfileImage, isValidNickname, isValidPassword, isPasswordMatch);
+        setIsFormValid(isFormValidNow);  // 유효성 검사 통과 여부 업데이트
+    }, [isValidNickname, isValidPassword, isPasswordMatch, isValidProfileImage]);  // 의존성 배열에 유효성 검사 상태 추가
+
+
+    // 닉네임 중복 검사 함수
+    const sendNickname = async (trimmedNickname: string) => {
+        try {
+            const response = await axiosInstance.get(
+                `${process.env.REACT_APP_API_URL}/api/v1/users/nickname?nickname=${trimmedNickname}`
+            );
+
+            if (response.status === 200) {
+                setIsValidNickname(true);  // 중복 검사에 통과했을 때 유효성 검사 통과
+                setNicknameError("사용 가능한 닉네임입니다.");
+                setShowNicknameSuccessToast(true);
+            }
+        } catch (error: any) {
+            if (error.response) {
+                console.log(error.response.message);
+                setIsValidNickname(false);  // 중복 검사에 실패했을 때 유효성 검사 실패
+                setNicknameError("사용할 수 없는 닉네임입니다.");
+                setShowNicknameErrorToast(true);
+            }
+        }
+    };
+
+    const handleIsNicknameAvailableButtonClick = async () => {
+        const trimmedNickname = nickname.trim();
+
+        if (trimmedNickname === currentUser.nickname) {
+            setIsValidNickname(true);  // 중복 검사에 통과했을 때 유효성 검사 통과
+            setNicknameError("사용 가능한 닉네임입니다.");
+            setShowNicknameSuccessToast(true);
+            return;
+        }
+
+        if (trimmedNickname) {
+            sendNickname(trimmedNickname);
+        }
+    };
+
+    const handleCloseNicknameSuccessToast = () => {
+        setShowNicknameSuccessToast(false);
+    };
+
+    const handleCloseNicknameErrorToast = () => {
+        setShowNicknameErrorToast(false);
+    };
+
+    const handleEditClick = () => {
+        setIsEditing(true); // 편집 모드로 전환
+    };
+
+    const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNicknameError("* 닉네임 중복 검사를 진행해주세요.");
+        const newNickname = e.target.value;
+        setNickname(newNickname);
+        const validationMessage = validateNickname(newNickname);
+        if (validationMessage === "사용 가능한 닉네임입니다.") {
+            // setNicknameError("");
+        } else {
+            setNicknameError(validationMessage);
+        }
+        setIsValidNickname(false);  // 닉네임을 수정하면 중복 검사를 다시 해야 하므로 false로 설정
+    };
+
+    const handleCancelClick = () => {
+        // 변경 내용 취소 시 초기 상태로 되돌리기
+        setIsEditing(false);
+        setNickname(currentUser.nickname);
+        setPassword("");
+        setConfirmPassword("");
+        setProfileImagePreview(currentUser.profileImage); // 원래 프로필 이미지로 돌아감
+    };
+
+    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+        const validationMessage = validateUserPassword(newPassword);
+        setPasswordError(validationMessage);
+        setIsValidPassword(validationMessage === "");
+    };
+
+    const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const newConfirmPassword = e.target.value;
+        setConfirmPassword(newConfirmPassword);
+        const validationMessage = validatePasswordMatch(password, newConfirmPassword);
+        setConfirmPasswordError(validationMessage);
+        setIsPasswordMatch(validationMessage === "");  // 비밀번호 일치 여부가 없을 경우 true
+    };
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files ? e.target.files[0] : null;
+
+        if (file) {
+            const validationError = validateProfileImage(file); // 유효성 검사
+            console.log(validationError);
+            setProfileImageError(validationError);
+            setIsValidProfileImage(validationError === "통과");  // 유효성 검사 통과 여부 업데이트
+
+            if (validationError === "통과") {
+                // 유효성 검사를 통과한 경우 이미지를 미리보기로 업데이트
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                    setProfileImagePreview(reader.result as string); // 이미지를 미리보기로 업데이트
+                };
+                reader.readAsDataURL(file); // 파일을 읽어서 base64 URL로 변환
+            } else {
+                // 유효성 검사를 통과하지 못한 경우 기존 프로필 이미지로 되돌림
+                setProfileImagePreview(currentUser?.profileImage || defaultProfileImage);
+            }
+        } else {
+            // 파일이 선택되지 않았을 경우 기본 이미지로 설정
+            setProfileImagePreview(currentUser?.profileImage || defaultProfileImage);
+            setProfileImageError(""); // 파일이 없을 때 기본 헬퍼 텍스트로 설정
+            setIsValidProfileImage(true); // 유효성 검사 실패
+        }
+    };
+
+
+    const handleSaveClick = () => {
+        if (isFormValid) {
+            setIsEditing(false); // 편집 모드 종료
+            // 서버에 저장 로직 추가 가능
+        }
+    };
 
     if (!currentUser) {
         return <div>Loading...</div>;
@@ -48,65 +204,130 @@ const MyInfoContainer: React.FC = () => {
                 <TabBar activeIndex={activeTab} setActiveIndex={setActiveTab} tabs={tabData} pageType="myInfo" />
             </div>
             {/* 프로필 섹션 */}
-            <div className="w-full max-w-3xl p-8 rounded-lg flex flex-col items-center justify-center">
+            <div className="w-full max-w-3xl p-8 rounded-full flex flex-col items-center justify-center">
                 <div className="w-[50%] flex flex-col items-center">
                     {/* 프로필 사진 */}
-                    <div className="flex flex-col items-center mb-8 w-full">
-                        <div className="w-full text-left mb-2">
-                            <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-lg shadow-md">
-                                프로필사진
-                            </span>
-                        </div>
-                        <div className="bg-gray-100 rounded-full shadow-md">
-                            <img
-                                src={currentUser?.profileImage ? currentUser.profileImage : `${process.env.PUBLIC_URL}/assets/images/gunssakdo.png`}
-                                alt="프로필사진"
-                                className="w-32 h-32 rounded-full object-fit"
-                            />
-                        </div>
+                    <div className="container mx-auto flex flex-col items-center">
+                        <ProfileSection
+                            profileImagePreview={profileImagePreview}
+                            handleFileChange={handleFileChange}
+                            profileImageError={profileImageError}
+                            currentUser={currentUser}
+                            defaultProfileImage={defaultProfileImage}
+                            isEditing={isEditing}
+                        />
                     </div>
 
-                    {/* 닉네임 */}
-                    <div className="mb-8 w-full">
-                        <div className="w-full text-left mb-2">
-                            <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-lg shadow-md">
-                                닉네임
-                            </span>
-                        </div>
-                        <span className="text-black">{currentUser.nickname}</span>
-                    </div>
+
+                    {/* 닉네임 섹션 */}
+                    <NicknameSection
+                        nickname={nickname}
+                        handleNicknameChange={handleNicknameChange}
+                        nicknameError={nicknameError}
+                        nicknameHelperTextColor={nicknameHelperTextColor}
+                        nicknameHelperText={nicknameHelperText}
+                        handleIsNicknameAvailableButtonClick={handleIsNicknameAvailableButtonClick}
+                        isEditing={isEditing}
+                        currentUser={currentUser}
+                    />
+
+                    {showNicknameSuccessToast && (
+                        <ToastNotification
+                            message="사용 가능한 닉네임"
+                            isSuccess={true}
+                            onClose={() => setShowNicknameSuccessToast(false)}
+                        />
+                    )}
+                    {showNicknameErrorToast && (
+                        <ToastNotification
+                            message="사용 불가능한 닉네임"
+                            isSuccess={false}
+                            onClose={() => setShowNicknameErrorToast(false)}
+                        />
+                    )}
 
                     {/* 이메일 */}
-                    <div className="mb-8 w-full">
-                        <div className="w-full text-left mb-2">
-                            <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-lg shadow-md">
-                                이메일
+                    {!isEditing ? (
+                        <div className="mb-8 w-full">
+                            <div className="w-full text-left mb-2">
+                                <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-full shadow-md">
+                                    이메일
+                                </span>
+                            </div>
+                            <span className="text-black underline">
+                                {currentUser.email}
                             </span>
                         </div>
-                        <span className="text-black underline">
-                            {currentUser.email}
-                        </span>
-                    </div>
+                    ) : <span></span>}
 
                     {/* 스터디 누적 시간 */}
-                    <div className="mb-8 w-full mb-[50px]">
-                        <div className="w-full text-left mb-2">
-                            <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-lg shadow-md">
-                                스터디 누적 시간
+                    {!isEditing ? (
+                        <div className="mb-8 w-full mb-[50px]">
+                            <div className="w-full text-left mb-2">
+                                <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-full shadow-md">
+                                    스터디 누적 시간
+                                </span>
+                            </div>
+                            <span className="text-black">
+                                24시간 53분
                             </span>
                         </div>
-                        <span className="text-black">
-                            24시간 53분
-                        </span>
+                    ) : <span></span>}
+
+                    {/* 비밀번호 섹션 */}
+                    <PasswordSection
+                        password={password}
+                        confirmPassword={confirmPassword}
+                        handlePasswordChange={handlePasswordChange}
+                        handleConfirmPasswordChange={handleConfirmPasswordChange}
+                        passwordError={passwordError}
+                        confirmPasswordError={confirmPasswordError}
+                        isEditing={isEditing}
+                    />
+
+                    {/* 버튼 섹션 */}
+                    <div className="flex space-x-4">
+                        {isEditing ? (
+                            <>
+                                <button
+                                    onClick={handleCancelClick}
+                                    className="bg-red-500 text-white font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-red-600"
+                                >
+                                    취소
+                                </button>
+                                <button
+                                    onClick={handleSaveClick}
+                                    disabled={!isFormValid} // 폼이 유효하지 않으면 버튼 비활성화
+                                    className={`px-6 py-2 rounded-full shadow-lg font-semibold ${isFormValid ? 'bg-[#4659AA] text-white hover:bg-[#3b4a99]' : 'bg-gray-400 text-gray-300 cursor-not-allowed'}`}
+                                >
+                                    수정완료
+                                </button>
+                            </>
+                        ) : (
+                            <button
+                                onClick={handleEditClick}
+                                className="bg-[#4659AA] text-white font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-[#3b4a99]"
+                            >
+                                정보 수정
+                            </button>
+                        )}
                     </div>
-
-                    {/* 정보 수정 버튼 */}
-                    <button className="bg-[#4659AA] text-white font-semibold px-6 py-2 rounded-full shadow-lg hover:bg-[#3b4a99]">
-                        정보 수정
-                    </button>
                 </div>
-
             </div>
+            {showNicknameSuccessToast && (
+                <ToastNotification
+                    message="사용 가능한 닉네임"
+                    isSuccess={true}
+                    onClose={handleCloseNicknameSuccessToast}
+                />
+            )}
+            {showNicknameErrorToast && (
+                <ToastNotification
+                    message="사용 불가능한 닉네임"
+                    isSuccess={false}
+                    onClose={handleCloseNicknameErrorToast}
+                />
+            )}
         </div>
     );
 };
---
+++ src/components/MyInfo/NicknameSection.tsx
@@ -0,0 +1,79 @@
+import React, { ChangeEvent } from "react";
+
+interface NicknameSectionProps {
+    nickname: string;
+    handleNicknameChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    nicknameError: string;
+    nicknameHelperTextColor: string;
+    nicknameHelperText: string;
+    handleIsNicknameAvailableButtonClick: () => void;
+    isEditing: boolean;
+    currentUser: any;
+}
+
+const NicknameSection: React.FC<NicknameSectionProps> = ({
+    nickname,
+    handleNicknameChange,
+    nicknameError,
+    nicknameHelperTextColor,
+    nicknameHelperText,
+    handleIsNicknameAvailableButtonClick,
+    isEditing,
+    currentUser,
+}) => {
+    return (
+        <div className="mb-8 w-full">
+            <div className="w-full text-left mb-2">
+                <span className="text-[#4659AA] bg-[#EEEEFF] px-3 py-1 rounded-full shadow-md">
+                    닉네임
+                </span>
+            </div>
+            {isEditing ? (
+                <div className="relative">
+                    <input
+                        type="text"
+                        value={nickname}
+                        onChange={handleNicknameChange}
+                        className="w-full h-12 p-3 pr-24 text-gray-700 border-b border-gray-300 rounded-lg focus:outline-none focus:border-[#4558A9] hover:border-[#4558A9]"
+                        placeholder="새 닉네임을 입력하세요"
+                    />
+                    <p
+                        className={`absolute text-xs italic mt-2 ${
+                            nicknameError === "사용 가능한 닉네임입니다."
+                                ? "text-blue-500"
+                                : "text-red-500"
+                        }`}
+                    >
+                        {nicknameError}
+                    </p>
+                    <button
+                        className={`absolute right-2 top-1/2 transform -translate-y-1/2 font-bold py-1 px-2 rounded-full focus:outline-none ${
+                            nicknameError === "" ||
+                            nicknameError === "* 닉네임 중복 검사를 진행해주세요."
+                                ? "text-[#6D81D5] hover:text-[#4659AA] cursor-pointer"
+                                : "text-gray-400 cursor-not-allowed"
+                        }`}
+                        type="button"
+                        onClick={handleIsNicknameAvailableButtonClick}
+                        disabled={
+                            !(
+                                nicknameError === "" ||
+                                nicknameError === "* 닉네임 중복 검사를 진행해주세요."
+                            )
+                        }
+                    >
+                        중복확인
+                    </button>
+
+                    <p className={`${nicknameHelperTextColor} text-xs italic mt-2`}>
+                        {nicknameHelperText}
+                    </p>
+                </div>
+            ) : (
+                <span className="text-black">{currentUser.nickname}</span>
+            )}
+        </div>
+    );
+};
+
+export default NicknameSection;
